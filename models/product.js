2e05dfe8bbfc496334357b9e042538486a442a44
Vagner Bruno <vagnerbruno0699@gmail.com>
2025-10-24 17:06:05 -0300

    Inicializa a API de E-commerce (Express/Mongoose) e modela o esquema 'Product'.

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    description: {
+    type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0.01
+    },
+    stack: {
+        type: Number,
+        required: true,
+        default: 0,
+        min: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    timestamps: true 
+});
+const Product = mongoose.model('Product', ProductSchema);
+module.exports =  Product;
---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DB_URL =process.env.MONGO_URL;
+
+// Middleware
+
+app.use(express.json());
+
+// Database Connection
+mongoose.connect(DB_URL) 
+    .then(() => {
+        console.log('Connected to MongoDB');
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on localhost:3000`);
+        });
+    })
+    .catch(err =>{
+        console.error('Database connection error:', err);
+
+    });
+
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: 'E-commerce is run!!' });
+    });
